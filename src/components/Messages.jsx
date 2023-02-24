470ac3bc0b0a39d40ca7b510395ae02a2961a6f7
LakshikaDias <lakshika9504@gmail.com>
2023-02-24 09:10:51 +0530

    done

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
 
 import Message from "./Message";
 
+//chat eke all msg athulath wena component eka
 const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+  useEffect(() => {
+    //take the "chats" doc "messages" field data form firebase
+    const unsub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    //cleanup
+    return () => {
+      unsub();
+    };
+  }, [data.chatId]);
+
+  // console.log("messages", messages);
+
   return (
     <div className="messages">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {messages.map((m) => {
+        return <Message message={m} key={m.id} />;
+      })}
     </div>
   );
 };
